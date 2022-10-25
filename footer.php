d88b71b19e099de670983b65f1e0e39cc1e94055
Gabriel Rodrigues <gabriel.rodrigues.brito@gmail.com>
2022-10-25 11:16:09 -0300

    footer desktop/mobile

--- footer.php
+++ footer.php
@@ -1,9 +1,48 @@
+<footer class="container">
+    <div class="newsletter">
+        <h3>newsletter <span>sign up</span></h3>
+        <form>
+            <div class="input-wrapper">
+                <input type="email" placeholder="your e-mail">
+                <button>sign up <i class="fa-solid fa-arrow-right"></i></button>
+            </div>
+            <label>
+                <input type="checkbox" name="lgpd" id="inLgpd">
+                <span>Accept LGPD</span>
+            </label>
+        </form>
+    </div>
+    <div class="contacts">
+        <a href="" target="_blank">
+            <span>lisboa</span>
+            <address>rua major afonso palla n&deg 23 H<br>1495-001 algés</address>
+        </a>
+        <a class="phone" href="">
+            <span>phone</span><br>
+            <span>+351 211 914 676</span>
+        </a>
+        <div>
+            <a href="mailto:geral@lightdesign.pt">
+                geral@lightdesign.pt
+            </a>
+            <br>
+            <a href="mailto:projetos@lightdesign.pt">
+                projetos@lightdesign.pt
+            </a>
+        </div>
+        <div class="social">
+            <a href="https://instagram.com" target="_blank">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+            <a href="https://linkedin.com" target="_blank">
+                <i class="fa-brands fa-linkedin"></i>
+            </a>
+        </div>
+    </div>
+</footer>
 
-    <footer>
-        FOOTER DA PÁGINA
-    </footer>
-
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/header.js"></script>
-    <?php wp_footer(); ?>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/header.js"></script>
+<?php wp_footer(); ?>
 </body>
+
 </html>
