2236375312d9bd0cc4bdb0939d0fc9376f11b731
Gabriel Rodrigues <gabriel.rodrigues.brito@gmail.com>
2022-11-01 15:59:05 -0300

    pagina 404

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<main class="interna-404 interna container">
+    <h1>
+        <span>404</span><br>
+        página não encontrada
+    </h1>
+    <p>A página que você buscou não foi encontrada. Por favor, utilize o menu acima para navegar no site.</p>
+</main>
+
+<?php get_footer(); ?>
